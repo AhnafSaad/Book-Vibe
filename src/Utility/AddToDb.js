1f634052eb3bbd52ded2f0a9dd50c768711a0fb5
Ahnaf Sadik Saad <ahnafsadik01857@gmail.com>
2026-04-05 20:19:46 +0600

    Updated package-lock.json

--- src/Utility/AddToDb.js
+++ src/Utility/AddToDb.js
@@ -1,9 +1,11 @@
+import toast from 'react-hot-toast';
+
 const getStoredDb = () =>{
    const StoredBookStr = localStorage.getItem('readList');
    if(StoredBookStr){
     const StoredBook = JSON.parse(StoredBookStr);
     return StoredBook;
-   } 
+   }
   else{
     return [];
   }
@@ -12,12 +14,63 @@ const getStoredDb = () =>{
 const addToDb = (id) =>{
     const StoredBookDb = getStoredDb();
     if(StoredBookDb.includes(id)){
-        alert('Already added to the list');
+        toast.error('Already added to the read list!', {
+            duration: 3000,
+            position: 'top-center',
+        });
+        return false;
     }
     else{
         StoredBookDb.push(id);
         const data = JSON.stringify(StoredBookDb);
         localStorage.setItem('readList', data);
+        toast.success('Book added to read list successfully!', {
+            duration: 3000,
+            position: 'top-center',
+        });
+        return true;
     }
 }
-export { addToDb, getStoredDb} 
+
+const getStoredWishlist = () =>{
+   const StoredWishlistStr = localStorage.getItem('wishlist');
+   if(StoredWishlistStr){
+    const StoredWishlist = JSON.parse(StoredWishlistStr);
+    return StoredWishlist;
+   }
+  else{
+    return [];
+  }
+}
+
+const addToWishlistDb = (id) =>{
+    const readList = getStoredDb();
+    if(readList.includes(id)){
+        toast.error('This book is already in your read list!', {
+            duration: 3000,
+            position: 'top-center',
+        });
+        return false;
+    }
+
+    const StoredWishlistDb = getStoredWishlist();
+    if(StoredWishlistDb.includes(id)){
+        toast.error('Already added to the wishlist!', {
+            duration: 3000,
+            position: 'top-center',
+        });
+        return false;
+    }
+    else{
+        StoredWishlistDb.push(id);
+        const data = JSON.stringify(StoredWishlistDb);
+        localStorage.setItem('wishlist', data);
+        toast.success('Book added to wishlist successfully!', {
+            duration: 3000,
+            position: 'top-center',
+        });
+        return true;
+    }
+}
+
+export { addToDb, getStoredDb, addToWishlistDb, getStoredWishlist } 
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -2,10 +2,33 @@ import Footer from "./../../components/Footer/Footer";
 import Navbar from "./../../components/Header/Header";
 import React from "react";
 import { Outlet } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 const Root = () => {
     return (
         <div className="max-w-6xl mx-auto">
+        <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            toastOptions={{
+                duration: 3000,
+                style: {
+                    background: '#363636',
+                    color: '#fff',
+                },
+                success: {
+                    duration: 3000,
+                    theme: {
+                        primary: '#23BE0A',
+                        secondary: '#23BE0A',
+                    },
+                },
+                error: {
+                    duration: 3000,
+                },
+            }}
+        />
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
